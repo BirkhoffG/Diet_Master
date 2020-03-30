e2124940fb8a612e6f52a428ce1db3f279d96e13
BirkhoffG <294170179@qq.com>
2020-03-30 20:41:03 +0800

    Update views and urls

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -10,9 +10,9 @@ def register_view(request):
     return render(request, "register.html")
 
 
-def survey_view(request, user_id):
+def survey_view(request):
     return render(request, "./dashboard/survey.html")
 
 
-def profile_view(request, user_id):
+def profile_view(request):
     return render(request, "./dashboard/profile.html")
